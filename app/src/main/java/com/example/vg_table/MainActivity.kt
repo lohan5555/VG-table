5c1a38c772e7031216a2fd60a339176c159b02ec
Lohan Hébert <lohanhebert7@gmail.com>
2025-11-09 12:45:07 +0100

    NavigationBar

--- app/src/main/java/com/example/vg_table/MainActivity.kt
+++ app/src/main/java/com/example/vg_table/MainActivity.kt
@@ -6,12 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.vg_table.ui.theme.VGtableTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.DateRange
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +32,40 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             VGtableTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                var selectedItem by remember { mutableIntStateOf(0) }
+                val items = listOf("Saisons", "Recherche", "Favoris")
+                val selectedIcons = listOf(Icons.Filled.DateRange, Icons.Filled.Search, Icons.Filled.Favorite)
+                val unselectedIcons =
+                    listOf(Icons.Outlined.DateRange, Icons.Outlined.Search, Icons.Outlined.Favorite)
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    bottomBar = {
+                        NavigationBar(
+                            containerColor = Color.Red
+                        ) {
+                            items.forEachIndexed { index, item ->
+                                NavigationBarItem(
+                                    icon = {
+                                        Icon(
+                                            if (selectedItem == index) selectedIcons[index] else unselectedIcons[index],
+                                            contentDescription = item,
+                                        )
+                                    },
+                                    label = { Text(item) },
+                                    selected = selectedItem == index,
+                                    onClick = { selectedItem = index },
+                                )
+                            }
+                        }
+                    }
+                ) { innerPadding ->
+                    Text(
+                        text = "Uwu",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VGtableTheme {
-        Greeting("Android")
     }
 }
